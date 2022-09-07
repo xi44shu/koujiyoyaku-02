d1e484862bfae0f9019d2a1df7e05e75d3d751f8
joni-deppu <44shuxichuan@gmail.com>
2022-09-07 16:58:09 +0900

    エラーメッセージ記入

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,5 +5,5 @@ class Schedule < ApplicationRecord
   belongs_to :time_zone
 
   #空の投稿を保存できないようにする
-  validates :saize_id, :accuracy_id, :time_zone, numericality: { other_than: 1 }, presence: true
+  validates :saize_id, :accuracy_id, :time_zone, numericality: { other_than: 1 , message: "can't be blank"}, presence: true
 end
