db544399907b3e2e8688fdab35279c36c21d1e70
joni-deppu <44shuxichuan@gmail.com>
2022-09-08 15:13:03 +0900

    teams/new,create作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'details/index'
-  get 'teams/index'
   devise_for :users
   root to: 'schedules#index'
   resources :trading_companies
   resources :prime_contractors
   resources :public_utilities
+  resources :teams
 end
