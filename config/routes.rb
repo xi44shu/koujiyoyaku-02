f0193cdbd69e8f027aed66d46a4c8b4865bfb902
joni-deppu <44shuxichuan@gmail.com>
2022-09-08 15:18:49 +0900

    detail/new,create作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'details/index'
   devise_for :users
   root to: 'schedules#index'
   resources :trading_companies
   resources :prime_contractors
   resources :public_utilities
   resources :teams
+  resources :details
 end
