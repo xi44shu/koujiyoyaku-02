f3eb0c6a6d70b5a0944eab42d50d80eba7d3b093
joni-deppu <44shuxichuan@gmail.com>
2022-09-08 14:45:31 +0900

    public_utilities/new,create作成

--- app/controllers/public_utilities_controller.rb
+++ app/controllers/public_utilities_controller.rb
@@ -1,4 +1,25 @@
 class PublicUtilitiesController < ApplicationController
+
   def index
   end
+
+  def new
+    @publicUtility = PublicUtility.new
+  end
+
+  def create
+    @publicUtility = PublicUtility.new(publicUtility_params)
+    if @publicUtility.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def publicUtility_params
+    params.permit(:pu_name )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'details/index'
   get 'teams/index'
-  get 'public_utilities/index'
   devise_for :users
   root to: 'schedules#index'
   resources :trading_companies
   resources :prime_contractors
+  resources :public_utilities
 end
