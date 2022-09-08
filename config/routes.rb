56c0c7b60cdf4e706e53b68aca9a6dd7812a9c68
joni-deppu <44shuxichuan@gmail.com>
2022-09-08 14:39:41 +0900

    prime_contractor/new,create作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'details/index'
   get 'teams/index'
   get 'public_utilities/index'
-  get 'prime_contractors/index'
   devise_for :users
   root to: 'schedules#index'
   resources :trading_companies
+  resources :prime_contractors
 end
